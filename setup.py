d59341fe2fbd7de1c610f6ab0a0bcdf943c9a5b6
Raymond Yeh <yehjxraymond@gmail.com>
2019-08-29 23:00:52 +0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ devRequirements = [
 
 setuptools.setup(
     name="quantfolio",
-    version="0.1.0",
+    version="0.2.0",
     author="Raymond Yeh",
     author_email="ray@geek.sg",
     description="A small example package",
